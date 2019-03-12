546addc543b48e8dc64be47232fff057e0bcd224
Phoenix314159 <derivative7117@live.com>
2019-03-11 22:43:27 -0600

    added GradeStatistics

--- Program.cs
+++ Program.cs
@@ -7,12 +7,26 @@ using System.Threading.Tasks;
 namespace Grades2
 {
     class Program
-    {
+    { 
+        static void GIveBookAName(GradeBook book)
+        {
+            book.Name = "The GradeBook";
+        }
         static void Main(string[] args)
         {
+            
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
             GradeBook book = new GradeBook();
             book.AddGrade(91f);
             book.AddGrade(89.5f);
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.HighestGrade);
+
         }
     }
 }
